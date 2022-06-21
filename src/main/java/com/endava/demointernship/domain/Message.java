7860c2b6dc12368603d3ffff25726dc4b1156073
mflores <martin.flores@endava.com>
2022-06-21 20:54:47 -0300

    Use autogenerated columns

--- src/main/java/com/endava/demointernship/domain/Message.java
+++ src/main/java/com/endava/demointernship/domain/Message.java
@@ -8,25 +8,20 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "MESSAGES")
 @Data
 @EntityListeners(AuditingEntityListener.class)
 public class Message {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "ID")
 	private Long id;
 
-	@Column(name = "MESSAGE")
 	private String message;
 
 	@ManyToOne
-	@JoinColumn(name="USER_ID")
 	private User user;
 
 
-	@Column(name = "CREATED_AT")
 	@CreatedDate
 	private LocalDateTime createdAt;
 }
--- src/main/java/com/endava/demointernship/domain/User.java
+++ src/main/java/com/endava/demointernship/domain/User.java
@@ -6,24 +6,16 @@ import javax.persistence.*;
 import java.util.List;
 
 @Entity
-@Table(name = "USERS")
 @Data
 public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "ID")
 	private Long id;
 
-	@Column(name = "NICKNAME")
 	private String nickname;
 
 	@ManyToMany
-	@JoinTable(
-			name = "USER_FOLLOWS",
-			joinColumns = @JoinColumn(name = "USER_ID"),
-			inverseJoinColumns = @JoinColumn(name = "FOLLOW_USER_ID")
-	)
 	private List<User> follows;
 
 }
